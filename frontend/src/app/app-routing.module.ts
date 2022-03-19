61c8b777930098fedd98de66f3ee653aaed1d77e
Kris Zhao <kriszhao@comcast.net>
2022-03-19 00:08:02 -0400

    Created all application pages for navbar routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountPageComponent } from './account-page/account-page.component';
+import { HistoryPageComponent } from './history-page/history-page.component';
+import { InputPageComponent } from './input-page/input-page.component';
+import { SummaryPageComponent } from './summary-page/summary-page.component';
+import { VisualizePageComponent } from './visualize-page/visualize-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'visualize',
+    component: VisualizePageComponent
+  },
+  {
+    path: 'history',
+    component: HistoryPageComponent
+  },
+  {
+    path: 'input',
+    component: InputPageComponent
+  },
+  {
+    path: 'account',
+    component: AccountPageComponent,
+  },
+  {
+    path: '',
+    component: SummaryPageComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AccountPageComponent } from './account-page/account-page.component';
+import { SummaryPageComponent } from './summary-page/summary-page.component';
+import { VisualizePageComponent } from './visualize-page/visualize-page.component';
+import { HistoryPageComponent } from './history-page/history-page.component';
+import { InputPageComponent } from './input-page/input-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AccountPageComponent,
+    SummaryPageComponent,
+    VisualizePageComponent,
+    HistoryPageComponent,
+    InputPageComponent
   ],
   imports: [
     BrowserModule,
